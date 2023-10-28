b04649809ce1a8376d02955d93f640d800fb2607
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-10-28 23:10:27 +0200

    set up basic e2e config

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require("cypress");
+require("dotenv").config();
+console.log(process.env); // remove this after you've confirmed it is working
 
 module.exports = defineConfig({
+	env: {
+		name: process.env.CY_NAME,
+		email: process.env.CY_EMAIL,
+		password: process.env.CY_PASSWORD,
+	},
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
 		baseUrl: "http://localhost:8080/",
 	},
+	waitForAnimations: true,
+	animationDistanceThreshold: 1,
+	responseTimeout: 30000,
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,52 @@
 describe("template spec", () => {
+	const name = Cypress.env("name");
+	const email = Cypress.env("email");
+	const password = Cypress.env("password");
+
+	beforeEach(() => {
+		cy.visit("/", { timeout: 30000 });
+	});
+
 	it("goes to the home page", () => {
-		cy.visit("/");
-		cy.get("footer").contains("Noroff FEU");
+		cy.get("#registerModalLabel").should("be.visible");
+	});
+
+	it("does not log in with wrong credentials", () => {
+		cy.get("#registerModalLabel").contains("Create Profile").should("be.visible");
+		cy.get("#registerModalLabel").contains("Create Profile").click(); // workaround for login button not being clickable in cypress
+		cy.get("#registerForm").contains("Login").click();
+
+		cy.get("#loginModalLabel").contains("Login").should("be.visible");
+
+		cy.wait(300);
+		cy.get("#loginEmail").click();
+		cy.get("#loginEmail").type(email);
+		cy.get("#loginPassword").click();
+		cy.get("#loginPassword").type("wrong_password");
+		cy.get("#loginForm > .modal-footer > .btn-success").click();
+
+		cy.on("window:alert", (str) => {
+			expect(str).to.equal(`Either your username was not found or your password is incorrect`);
+		});
+	});
+
+	it("logs in", () => {
+		cy.get("#registerModalLabel").contains("Create Profile").should("be.visible");
+		cy.get("#registerModalLabel").contains("Create Profile").click(); // workaround for login button not being clickable in cypress
+		cy.get("#registerForm").contains("Login").click();
+
+		cy.get("#loginModalLabel").contains("Login").should("be.visible");
+
+		cy.wait(300);
+		cy.get("#loginEmail").click();
+		cy.get("#loginEmail").type(email);
+		cy.get("#loginPassword").click();
+		cy.get("#loginPassword").type(password);
+		cy.get("#loginForm > .modal-footer > .btn-success").click();
+
+		cy.get("#loginModalLabel").should("not.be.visible");
+		cy.url().should("include", "?view=profile&name=noroff_test_user");
+		cy.get("h4").contains("noroff_test_user").should("be.visible");
+		cy.get("span").contains("noroff-test-user@noroff.no").should("be.visible");
 	});
 });
